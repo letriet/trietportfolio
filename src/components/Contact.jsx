806b1c726c1316974cf658d8df9f5fae3c6face1
Triet <leetriets@gmail.com>
2022-11-15 05:35:16 +0900

    please

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       className="w-full bg-[#000009] flex justify-center items-center h-screen"
     >
       
-      <form method='POST' action="https://getform.io/f/23bc1d5b-4ee8-4945-96af-f58f62f26050" className="flex flex-col max-w-[550px] w-full h-screen py-20">
+      <form method='POST' action="https://getform.io/f/23bc1d5b-4ee8-4945-96af-f58f62f26050" className="flex flex-col max-w-[550px] w-full h-screen">
         <div className="pb-2">
           <p className="text-4xl font-bold text-gray-300 pt-14 pb-4 ">
             Contact
