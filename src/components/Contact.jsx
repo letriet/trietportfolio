8c9133996811225f48b62bbbb5af791de6372d12
Triet <leetriets@gmail.com>
2022-11-15 05:19:44 +0900

    final final hopefully

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,12 +6,12 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-[#000009] flex justify-center items-center p-20"
+      className="w-full bg-[#000009] flex justify-center items-center h-screen"
     >
       
-      <form method='POST' action="https://getform.io/f/23bc1d5b-4ee8-4945-96af-f58f62f26050" className="flex flex-col max-w-[550px] w-full">
+      <form method='POST' action="https://getform.io/f/23bc1d5b-4ee8-4945-96af-f58f62f26050" className="flex flex-col max-w-[550px] w-full h-3/4 py-10">
         <div className="pb-2">
-          <p className="text-4xl font-bold inline border-b-4 border-[#7290A9] text-gray-300">
+          <p className="text-4xl font-bold inline border-b-4 border-[#7290A9] text-gray-300 py-20">
             Contact
           </p>
           <p className="py-4 text-gray-300">For Mobile Users, click below or send a form</p>
@@ -42,7 +42,7 @@ const Contact = () => {
         <input className="bg-[#7290A9] p-2 placeholder-black" type="text" placeholder='Name' name="name" />
         <input className="my-4 p-2 bg-[#7290A9] placeholder-black" type="email" placeholder='Email' name="email" />
         <textarea className="bg-[#7290A9] p-2 placeholder-black" name="message" rows="10" placeholder='Message'></textarea> 
-        <button className="text-gray-300 hover:bg-[#215B7A] border-2 rounded-2xl px-4 py-3 my-8 mx-auto flex items-center border-[#215B7A]">Let's Connect</button>
+        <button className="text-gray-300 hover:bg-[#215B7A] border-2 rounded-2xl px-4 py-3 my-10  mx-auto flex items-center border-[#215B7A]">Let's Connect</button>
       </form>
     </div>
   );
